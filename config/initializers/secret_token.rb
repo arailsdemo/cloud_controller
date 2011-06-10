54542613d06e230ffc97482904be7a4d4460cf40
aRailsDemo <arailsdemo@gmail.com>
2011-06-10 15:32:49 -0700

    [initial commit]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vcap::Application.config.secret_token = '1d2c594bcee10dc2afc51e84fa2c0ffea2902405d011e77bdac478f8a13bed9c9e1aeccd3a563be7f1edfcc250f4144807b2f197b167e93e713ec0c3de5f1997'
