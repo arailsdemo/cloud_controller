888aa705ce6c35185a01c0071eb1222e9a3bf5c9
aRailsDemo <arailsdemo@gmail.com>
2011-06-10 16:05:52 -0700

    UserToken#to_json

--- app/models/user_token.rb
+++ app/models/user_token.rb
@@ -1,5 +1,7 @@
 class UserToken
   class << self
+    attr_accessor :token_key
+
     def create(username)
       valid_until = (Time.now.utc + 1.week).to_i
       new(username, valid_until)
@@ -11,5 +13,20 @@ class UserToken
   def initialize(name, time)
     @username = name
     @valid_until = time
+  end
+
+  def to_json(options=nil)
+    Yajl::Encoder.encode({"token" => encoded_token})
+  end
+
+  def encoded_token
+    data = [username, valid_until, hmac]
+    Marshal.dump(data).unpack('H*').first
+  end
+
+  def hmac
+    @hmac ||= HMAC::SHA1.new(self.class.token_key)
+                        .update([username, valid_until].join)
+                        .digest
   end
 end
--- spec/models/user_token_spec.rb
+++ spec/models/user_token_spec.rb
@@ -9,4 +9,13 @@ describe UserToken do
       Timecop.return
     end
   end
+
+   describe "#to_json" do
+    it "encodes and converts the token to json" do
+      UserToken.should_receive(:token_key) { 'abc' }
+      token = klass.new('foo@bar.com', Time.new(2011))
+      token.to_json.should == { "token" => "04085b08492210666f6f406261722e636f6d063a06454649753a0954696d650d28c01b8000000000073a0b405f7a6f6e6522085053543a0b6f666673657469fe808f2219d9144830c63ee6bea2cde03ff056bc8505d9d50e"
+                              }.to_json
+    end
+  end
 end
